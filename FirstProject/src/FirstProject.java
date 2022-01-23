23ed6c22c1a04db68909873c285b6b8891a255d7
Shahril3001 <Shahril3001.SR@gmail.com>
2022-01-23 20:48:53 +0800

    Second Testing

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -1,4 +1,6 @@
 
 public class FirstProject {
-	public static void main(String[] args) { System.out.println("Hello, world!"); }
+	public static void main(String[] args) {
+		System.out.println("Hello Shahril3001!"); 
+		}
 }
